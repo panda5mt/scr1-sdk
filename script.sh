5a993d280831eb28236b56f9f14113f043c8a9a0
lynxeyed <lynxeyed@live.jp>
2020-04-04 14:05:52 +0900

    Added script Compile C-source on GCC and Compile SCR1 Soft-Core FPGA Logic on Quartus Prime 19.1

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eu
+# Compile SCR1 C code on GCC and build SCR1 Soft-core CPU logic Quartus Prime (need wsl on Win10)
+export PATH=$PATH:/opt/riscv/bin
+C_PATH=/opt/riscv/bin
+QUARTUS_ROOTDIR=/mnt/c/intelFPGA_lite/19.1/quartus/bin64
+SOPC_ROOTDIR=/mnt/c/intelFPGA_lite/19.1/quartus/sopc_builder/bin
+DESIGN_NAME=cy10lp
+SW_DESIGN_NAME=de10lite_scr1
+
+RISCV_PROJ_DIR=/mnt/c/RISCV/Syntacore/scr1-sdk
+RISCV_WINDIR=`wslpath -w ${RISCV_PROJ_DIR}`
+
+SVFDIR=${RISCV_PROJ_DIR}/fpga/cy10lp/scr1/output
+SVFDIRW=`wslpath -w ${SVFDIR}`
+QSYSDIR=${RISCV_PROJ_DIR}/fpga/cy10lp/scr1/qsys
+QSYSDIRW=`wslpath -w ${QSYSDIR}`
+SWDIR=${RISCV_PROJ_DIR}/sw/sc-bl
+SWDIRW=`wslpath -w ${SWDIR}`
+
+cd ${SWDIR}
+make deep_clean
+make CROSS_COMPILE=riscv64-unknown-elf- PLATFORM=${SW_DESIGN_NAME}
+
+
+cd ${RISCV_PROJ_DIR}/fpga/cy10lp/scr1
+#	${QUARTUS_ROOTDIR}/quartus_sh.exe --flow clean cy10lp
+${QUARTUS_ROOTDIR}/quartus_sh.exe --flow compile cy10lp
+cd ${SVFDIR}
+${QUARTUS_ROOTDIR}/quartus_cpf.exe -c -n p -q 2MHz -g 3.3 ${SVFDIRW}/${DESIGN_NAME}.sof ${SVFDIRW}/${DESIGN_NAME}.svf
+cd ${QSYSDIR}
+
+${SOPC_ROOTDIR}/sopcinfo2swinfo.exe --input=${QSYSDIRW}/${DESIGN_NAME}_qsys.sopcinfo
+${SOPC_ROOTDIR}/swinfo2header.exe --swinfo ${QSYSDIRW}/${DESIGN_NAME}_qsys.swinfo --module avl_dmem --single ${SWDIRW}/src/qsys_mem_map.h 
+
+
+# Copy build *.SVF file to your Raspberry Pi(for example:Pi on 192.168.1.10)
+scp -r ${SVFDIR}/${DESIGN_NAME}.svf pi@192.168.1.10:/home/pi
